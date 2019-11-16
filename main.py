3b644616cfc69f67cd1ced6badcaf478e7539b23
narakuf <lgd9598@gmail.com>
2019-11-16 02:13:43 -0500

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn import ensemble
 from sklearn.decomposition import PCA
 from sklearn import neural_network
 
@@ -42,16 +42,17 @@ if __name__ == '__main__':
 
     # baseline
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    reg = linear_model.LinearRegression()
+    # reg = linear_model.LinearRegression()
 
     # build a ml pipeline
-    '''reg = make_pipeline(# PolynomialFeatures(),
+    reg = make_pipeline(# PolynomialFeatures(interaction_only=True),
                         # PCA(n_components='mle'),
                         # SelectPercentile(f_regression, percentile=20),
                         StandardScaler(),
+                        linear_model.LinearRegression())
                         # neural_network.MLPRegressor())
                         # linear_model.SGDRegressor(penalty='l1', l1_ratio=0.8, max_iter=1000, random_state=11, learning_rate='optimal'))
-                        linear_model.LassoCV(cv=3))'''
+                        # ensemble.AdaBoostRegressor(linear_model.LassoCV(cv=10, n_jobs=-1), loss='exponential'))
 
     # train
     reg.fit(X_train, y_train)
@@ -62,8 +63,7 @@ if __name__ == '__main__':
     # round predictions to integer
     # y_pred = np.around(y_pred)
     mae = mean_absolute_error(y_test, y_pred)
-    plt.scatter(y_test, y_pred)
-    plt.show()
+
     min_year = np.min(y_test[:, 0])
     max_year = np.max(y_test[:, 0])
     y_mean = np.empty(shape=[max_year - min_year + 1, 2])
